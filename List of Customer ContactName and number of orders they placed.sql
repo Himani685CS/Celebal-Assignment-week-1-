3db68da9944366af77c5638674d5b33a6e597de7
HIMANI SUJANE <121811029+Himani685CS@users.noreply.github.com>
2024-06-07 18:13:39 +0530

    Create List of Customer ContactName and number of orders they placed.sql

---
+++ List of Customer ContactName and number of orders they placed.sql
@@ -0,0 +1,11 @@
+SELECT 
+    p.FirstName + ' ' + p.LastName AS ContactName,
+    COUNT(soh.SalesOrderID) AS NumberOfOrders
+FROM 
+    Sales.Customer AS c
+JOIN 
+    Person.Person AS p ON c.PersonID = p.BusinessEntityID
+JOIN 
+    Sales.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID
+GROUP BY 
+    p.FirstName, p.LastName;
